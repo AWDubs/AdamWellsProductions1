ab7a080b9304cba7004fb74714ceba2d75711593
wellsadam16089 <wellsadam82@gmail.com>
2021-08-09 19:31:15 -0500

    Updates

---
+++ precache-manifest.f536fb236f9872ba8249481751172a4f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a423101b146a535abb0e25d22c96c6b",
+    "url": "/AdamWellsProductions/index.html"
+  },
+  {
+    "revision": "79d323020d0387479f9c",
+    "url": "/AdamWellsProductions/static/css/main.fcfc372b.chunk.css"
+  },
+  {
+    "revision": "071835e712b2c93b11b1",
+    "url": "/AdamWellsProductions/static/js/2.ece952e9.chunk.js"
+  },
+  {
+    "revision": "79d323020d0387479f9c",
+    "url": "/AdamWellsProductions/static/js/main.d6f31698.chunk.js"
+  },
+  {
+    "revision": "f64f737b3ab3b1d15564",
+    "url": "/AdamWellsProductions/static/js/runtime-main.41d82a9b.js"
+  },
+  {
+    "revision": "479b0d0e17e1c14d82877cf29fb0b1b4",
+    "url": "/AdamWellsProductions/static/media/AW.479b0d0e.png"
+  },
+  {
+    "revision": "1ab427420a148b192fd82f889d1a23d8",
+    "url": "/AdamWellsProductions/static/media/Adam.1ab42742.jpg"
+  },
+  {
+    "revision": "86ee100eaf6cd07638b98248e1432228",
+    "url": "/AdamWellsProductions/static/media/all.86ee100e.png"
+  },
+  {
+    "revision": "45fffff315924e7897503706be70b71f",
+    "url": "/AdamWellsProductions/static/media/comp.45fffff3.jpg"
+  },
+  {
+    "revision": "035976d30c49c323e435ca0590159bff",
+    "url": "/AdamWellsProductions/static/media/email.035976d3.png"
+  },
+  {
+    "revision": "92fa16fcd8f8f3fa996cb2896d90c846",
+    "url": "/AdamWellsProductions/static/media/phonecomputer.92fa16fc.png"
+  }
+]);
